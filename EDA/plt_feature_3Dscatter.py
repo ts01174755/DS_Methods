6b6f2e8dd9d18d7f0806567aa1fb53593ba3cdf1
Wu,Pei-Yu <ts01174755@gmail.com>
2020-11-21 09:34:38 +0800

    Create plt_feature_3Dscatter.py

---
+++ EDA/plt_feature_3Dscatter.py
@@ -0,0 +1,31 @@
+# plt-3D
+train['3D_axis1'] = train.loc[:,['open_count_last_10_days']]
+train['3D_axis2'] = train.loc[:,['open_count_last_30_days']]
+train['3D_axis3'] = train.loc[:,['open_count_last_60_days']]
+
+ax = plt.figure(figsize = (16,10)).gca(projection='3d')
+ax.view_init(30, -20)
+ax.scatter(
+    xs = train.loc[:,'3D_axis1'],
+    ys = train.loc[:,'3D_axis2'],
+    zs = train.loc[:,'3D_axis3'],
+    c = train.loc[:,'open_flag'],
+    cmap = 'tab10')
+ax.set_xlabel('3D_axis1')
+ax.set_ylabel('3D_axis2')
+ax.set_zlabel('3D_axis3')
+plt.show
+
+
+ax = plt.figure(figsize = (16,10)).gca(projection='3d')
+ax.view_init(30, -110)
+ax.scatter(
+    xs = train.loc[:,'3D_axis1'],
+    ys = train.loc[:,'3D_axis2'],
+    zs = train.loc[:,'3D_axis3'],
+    c = train.loc[:,'open_flag'],
+    cmap = 'tab10')
+ax.set_xlabel('3D_axis1')
+ax.set_ylabel('3D_axis2')
+ax.set_zlabel('3D_axis3')
+plt.show
