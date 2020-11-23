541a601ac12b1ffe1fb222eac7a177bf3cf5f6e7
Wu,Pei-Yu <ts01174755@gmail.com>
2020-11-23 17:22:17 +0800

    Update df_get_cdf.py

--- Feature/df_get_cdf.py
+++ Feature/df_get_cdf.py
@@ -12,7 +12,7 @@ from collections import defaultdict
 #%%
 def df_get_cdf(df,X,Y,ascending=True):
     def zero():
-        return 0
+        return 0    
     # df sort 
     df_sort = df.sort_values(by=[X],ascending=ascending).reset_index(drop=True)
     arr_len = df_sort.shape[0]
@@ -35,27 +35,40 @@ def df_get_cdf(df,X,Y,ascending=True):
     for fi_,fv_ in enumerate(feat_N):
         PY_arr[:,fi_] = PY_arr[:,fi_] / tmp_N[fv_]
 
-    df_cdf = pd.concat([df_sort,pd.DataFrame(PY_arr,columns=columns)],axis=1)
+    df_cdf = pd.concat([df_sort[X],
+                        pd.DataFrame(PY_arr,columns=columns)],axis=1)\
+                .groupby(by=[X]).max()
     
-    return df_cdf
+    return df_sort.merge(df_cdf, left_on=X, right_on=X)
+
 #%%
 if __name__ == "__main__":
-    mu, sigma = 10, 5
+    mu, sigma = 20, 5
     s1 = st.norm(mu, sigma).rvs(100000)
-    s2 = st.norm(mu-4, sigma+4).rvs(10000)
+    s2 = st.norm(mu-24, sigma+4).rvs(10000)
 
     df_1 = pd.DataFrame(s1,columns=["feat"]  )
     df_1['Y'] = 0
+    
     df_2 = pd.DataFrame(s2,columns=["feat"]  )
     df_2['Y'] = 1
     df = pd.concat([df_1,df_2],axis=0).reset_index(drop=True)
-    
+    df = round(df,0)
     df_cdf = df_get_cdf(df, X="feat", Y="Y", ascending=True)
-    #df_cdf = df_get_cdf(df_cdf, X="feat", Y="Y", ascending=False)
-    #df_cdf['cdf_delta'] = df_cdf["Des_cdf(Y=1)"] - df_cdf["As_cdf(Y=0)"]
-    df_cdf["InfoGain_Y=1"] = df_cdf["As_cdf(Y=1)"]*np.log2(df_cdf["As_cdf(Y=1)"])
-    df_cdf["InfoGain_Y=0"] = df_cdf["As_cdf(Y=0)"]*np.log2(df_cdf["As_cdf(Y=0)"])
-    df_cdf["InfoGain_Y=0"] = df_cdf["InfoGain_Y=0"].fillna(0)
     
-    #sns.pairplot(df_cdf.sample(n=1000),hue="Y")
+    
+    plt.figure(figsize=(15,10),dpi=100,linewidth = 2)
+    plt.plot(df_cdf['feat'],df_cdf['As_cdf(Y=1)'],'s-',color = 'r', label="TSMC")
+    plt.plot(df_cdf['feat'],df_cdf['As_cdf(Y=0)'],'o-',color = 'g', label="FOXCONN")
+    
+    plt.xticks(fontsize=20)
+    plt.yticks(fontsize=20)
+    
+    # 標示x,y軸(labelpad代表與圖片的距離)
+    plt.xlabel("feat", fontsize=30, labelpad = 15)
+    plt.ylabel("As_cdf", fontsize=30, labelpad = 20)
+    
+    # 顯示出線條標記位置
+    plt.legend(loc = "best", fontsize=20)
+    plt.show()
     
