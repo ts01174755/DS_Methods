7b8b7f2070adb89d3f5baabd7b64bb29e6dea63c
Wu,Pei-Yu <ts01174755@gmail.com>
2020-11-25 11:37:10 +0800

    Update df_get_pdf.py

--- Feature/df_get_pdf.py
+++ Feature/df_get_pdf.py
@@ -59,18 +59,18 @@ def df_get_pdf(df,X,Y,X_len,delta=None,CDF_Monotonic=True):
         return func_, X_Y[-2,0] - X_Y[-3,0]
     
     df_sort = df.sort_values(by=[X]).reset_index(drop=True)
-    feat_X = df_sort[X].to_numpy()
+    feat_X = df_sort[X].to_numpy()  #格式轉換
 
     interval_n = math.ceil((feat_X.max()-feat_X.min())/X_len)
-    interval_v = [feat_X.min() + (i-0.5)*X_len for i in range(interval_n+1)]
-    for i in range(interval_n):
-        feat_X[(interval_v[i] <= feat_X) & (feat_X < interval_v[i+1]) ] = \
-            (interval_v[i] + interval_v[i+1])/2
-    df_sort[X] = feat_X
-
+    feat_Xmin = feat_X.min()
+    for i in range(interval_n,-1,-1):
+        feat_X[(feat_Xmin + (i)*X_len <= feat_X) &\
+               (feat_X < feat_Xmin + (i+1)*X_len) ] = feat_Xmin + (i+1)*X_len
+    
     feat_cdf = feature_cdf(df_sort, X=X, Y=Y)
     cols = feat_cdf.columns    
-    
+            
+    df_sort[X] = df_sort[X] - X_len/2
     if delta == None:
         delta = X_len/2
     
@@ -87,13 +87,12 @@ def df_get_pdf(df,X,Y,X_len,delta=None,CDF_Monotonic=True):
     
     return df_sort
 
-
 #%%
 if __name__ == "__main__":
     #data prepare
     mu, sigma = 10, 5
     s1 = st.norm(mu, sigma).rvs(100000)
-    s2 = st.norm(mu-4, sigma+4).rvs(10000)
+    s2 = st.norm(mu-4, sigma+4).rvs(100000)
 
     df_1 = pd.DataFrame(s1,columns=["feat"]  )
     df_1['Y'] = 0
@@ -102,27 +101,31 @@ if __name__ == "__main__":
     df_2['Y'] = 1
     df = pd.concat([df_1,df_2],axis=0).reset_index(drop=True)
     
-    # df_get_pdf
-    df_pdf = df_get_pdf(df, X='feat', Y='Y', X_len=.1, delta=.1,CDF_Monotonic=True)
-    df_pdf['pdf_diff'] = df_pdf['pdf(Y=1)'] - df_pdf['pdf(Y=0)']
+#%%
+X_len = 5
+delta = .5
+X = 'feat'
+Y = 'Y'
+bins = math.ceil((df[X].max()-df[X].min())/X_len)-2
 
-    plt.figure(figsize=(15,10),dpi=100,linewidth = 2)
-    plt.hist(df.loc[df['Y']==0,'feat'],bins=70,density=True)
-    plt.hist(df.loc[df['Y']==1,'feat'],bins=70,density=True)
-    plt.show()
-    #plt
-    plt.figure(figsize=(15,10),dpi=100,linewidth = 2)
-    plt.plot(df_pdf['feat'],df_pdf['pdf(Y=1)'],'-',color = 'r', label="pdf(Y=1)")
-    plt.plot(df_pdf['feat'],df_pdf['pdf(Y=0)'],'-',color = 'g', label="pdf(Y=0)")
-    #plt.plot(df_pdf['feat'],df_pdf['pdf_diff'],'-',color = 'b', label="pdf_diff")
-    
-    plt.xticks(fontsize=20)
-    plt.yticks(fontsize=20)
-    
-    # 標示x,y軸(labelpad代表與圖片的距離)
-    plt.xlabel("feat", fontsize=30, labelpad = 15)
-    plt.ylabel("pdf", fontsize=30, labelpad = 20)
-    
-    # 顯示出線條標記位置
-    plt.legend(loc = "best", fontsize=20)
-    plt.show()
+# df_get_pdf
+df_pdf = df_get_pdf(df, X=X, Y=Y, X_len=X_len, delta=delta,CDF_Monotonic=True)
+df_pdf['pdf_diff'] = df_pdf['pdf(Y=1)'] - df_pdf['pdf(Y=0)']
+df[X].min()
+plt.figure(figsize=(15,10),dpi=100,linewidth = 2)
+plt.hist(df.loc[df[Y]==0,X],bins=bins,density=True)
+plt.hist(df.loc[df[Y]==1,X],bins=bins,density=True)
+plt.plot(df_pdf[X],df_pdf['pdf(Y=1)'],'o-',color = 'r', label="pdf(Y=1)")
+plt.plot(df_pdf[X],df_pdf['pdf(Y=0)'],'o-',color = 'g', label="pdf(Y=0)")
+#plt.plot(df_pdf['feat'],df_pdf['pdf_diff'],'-',color = 'b', label="pdf_diff")
+
+plt.xticks(fontsize=20)
+plt.yticks(fontsize=20)
+
+# 標示x,y軸(labelpad代表與圖片的距離)
+plt.xlabel("feat", fontsize=30, labelpad = 15)
+plt.ylabel("pdf", fontsize=30, labelpad = 20)
+
+# 顯示出線條標記位置
+plt.legend(loc = "best", fontsize=20)
+plt.show()
